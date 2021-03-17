297561839ac5e302dc03009ebc787fcf83df71a9
ricky0906 <rupinia.ricky@gmail.com>
2021-03-17 15:19:01 +0900

    メッセージの表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,6 +2,7 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @room = Room.find(params[:room_id])
+    get_messages
   end
 
   def create
@@ -10,6 +11,7 @@ class MessagesController < ApplicationController
     if @message.save
       redirect_to room_messages_path(@room)
     else
+      get_messages
       render :index
     end
   end
@@ -18,5 +20,9 @@ class MessagesController < ApplicationController
 
   def messages_params
     params.require(:message).permit(:content).merge(user_id: current_user.id)
+  end
+
+  def get_messages
+    @messages = @room.messages.includes(:user)
   end
 end
