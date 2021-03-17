47ca64a2eddca984f2b4182e9142b677431ae770
ricky0906 <rupinia.ricky@gmail.com>
2021-03-17 12:49:27 +0900

    ルーム初期データの挿入

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_17_032941) do
+ActiveRecord::Schema.define(version: 2021_03_17_033541) do
 
   create_table "rooms", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
