b75dc9a84e8468dd30cc9bb6d28b226b0b11c14d
ricky0906 <rupinia.ricky@gmail.com>
2021-03-19 23:46:27 +0900

    メッセージの削除機能

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root to: "rooms#index"
   resources :users, only: [:edit, :update]
   resources :rooms, only: [:create] do
-    resources :messages, only: [:index, :create]
+    resources :messages, only: [:index, :create, :destroy]
   end
 end
