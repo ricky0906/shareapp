bb1af87582171b32c1479a857017c01c37012cc3
ricky0906 <rupinia.ricky@gmail.com>
2021-04-01 11:59:49 +0900

    マイページにお気に入りを表示させるためのusersコントローラーの定義

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,14 @@ class UsersController < ApplicationController
   def edit
   end
 
+  def show
+    @user = User.find(params[:id])
+    @messages = @user.messages
+    
+    favorites = Favorite.where(user_id: current_user.id).pluck(:message_id)
+    @favorite_list = Message.find(favorites)
+  end
+
   def update
     if current_user.update(user_params)
       redirect_to root_path
